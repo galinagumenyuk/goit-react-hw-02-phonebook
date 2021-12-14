139062387ce411058fa9e6ff488d4c7e1eba0eca
galinagumenyuk <gymenyuk.galina@gmail.com>
2021-12-14 12:21:26 +0200

    basic markup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import "./App.css";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
+import React from "react";
+import shortid from "shortid";
 
-function App() {
-  return <div>test</div>;
+class App extends React.Component {
+  state = {
+    contacts: [],
+    name: "",
+  };
+
+  nameInputId = shortid.generate();
+
+  handleNameChange = (e) => {
+    this.setState({ name: e.currentTarget.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={this.state.name}
+              onChange={this.handleNameChange}
+              id={this.nameInputId}
+            />
+          </label>
+          <button type="submit"> Add contact </button>
+        </form>
+        <p>Contacts</p>
+      </div>
+    );
+  }
 }
 
 export default App;
